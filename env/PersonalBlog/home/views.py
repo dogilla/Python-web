327fe40ccfd66dcc607eeeafbdff7b98f67b3e53
dogilla <19thmagm@ciencias.unam.mx>
2019-09-07 22:54:19 -0500

    Practica 3

--- env/PersonalBlog/home/views.py
+++ env/PersonalBlog/home/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 
-def index(request):
-    return render(request,'home/index.html')
+def index(View):
+    return render(request,'index.html')
+
+
+class Index(View):
+    template = 'home/index.html'
+    context = {'title':'Index'}
+
+    def get(self, request):
+        return render(request, self.template, self.context)
+
+class About(View):
+    template = 'home/about.html'
+    context = {'title': 'About me'}
+    def get(self, request):
+        return render(request, self.template, self.context )
+
